150c6f7d1b82bfd1cb18e441476d2f8a1769bcde
root <gzhch@pku.edu.cn>
2024-01-15 19:31:47 +0800

    run.sh

--- run.sh
+++ run.sh
@@ -10,11 +10,8 @@
 
 for window in 30 60 120
 do
-    python decoding/layerwise_alignment.py --window window --roi mean_most:1000
-    python decoding/layerwise_alignment.py --window window --roi mean_least:1000
-    python decoding/layerwise_alignment.py --window window --roi mean_th_most:0.1
-    python decoding/layerwise_alignment.py --window window --roi std_most:1000
-    python decoding/layerwise_alignment.py --window window --roi std_least:1000
-    python decoding/layerwise_alignment.py --window window --roi std_th_most:0.2
-    python decoding/layerwise_alignment.py --window window --roi all
+    python decoding/layerwise_alignment.py --window $window --roi all
+    python decoding/layerwise_alignment.py --window $window --roi mean_most:1000 & python decoding/layerwise_alignment.py --window $window --roi mean_least:1000
+    python decoding/layerwise_alignment.py --window $window --roi mean_th_most:0.1 & python decoding/layerwise_alignment.py --window $window --roi std_most:1000
+    python decoding/layerwise_alignment.py --window $window --roi std_least:1000 & python decoding/layerwise_alignment.py --window $window --roi std_th_most:0.2
 done
